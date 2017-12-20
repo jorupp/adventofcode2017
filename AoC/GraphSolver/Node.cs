3fb6569e3a80171515a50424ab36117d50ca5c22
Jonathan Rupp <jorupp@gmail.com>
2017-12-19 18:44:50 -0600

    Move to VS...

---
+++ AoC/GraphSolver/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.GraphSolver
+{
+    public abstract class Node<TNode> where TNode : Node<TNode>
+    {
+        public abstract IEnumerable<TNode> GetAdjacent();
+        public abstract bool IsValid { get; }
+        public abstract bool IsComplete { get; }
+        public abstract int CurrentCost { get; }
+        public abstract int EstimatedCost { get; }
+        public abstract object[] Keys { get; }
+        public string Key { get { return string.Join("_", Keys); } }
+        public abstract string Description { get; }
+    }
+}
---
+++ AoC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AoC.Year2015.Day22;
+
+namespace AoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Part1().Run();
+            new Part2().Run();
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
